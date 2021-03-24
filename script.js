a26197b19fd4910e12e6104cee16ab6e081c44f1
mungadunga <67319507+mungadunga@users.noreply.github.com>
2021-03-24 21:31:50 +0100

    v2.0.1

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
+// dataset
 const dataset = [{
    "date": [],
    "grades": [],
-   "description": []
+   "description": [],
+   "coef": []
 }];
+
+// setup
 
 Object.size = function(obj){
    let size = 0;
@@ -10,10 +14,11 @@ Object.size = function(obj){
    return size;
 };
 
-let createGrade = (d1, g, d2) => {
-   dataset[0].date.push(d1);
-   dataset[0].grades.push(g);
-   dataset[0].description.push(d2);
+let createGrade = (date, grade, description, coef) => {
+   dataset[0].date.push(date);
+   dataset[0].grades.push(grade);
+   dataset[0].description.push(description);
+   Array.from({length: coef}, () => dataset[0].coef.push(grade));
 };
 
 let createAnnouncement = (title, content, color) => {
@@ -36,13 +41,24 @@ let createAnnouncement = (title, content, color) => {
 
 // create grade =>
    createGrade("Date", "Grade:", "Description:");
-   createGrade("16.03.21", 8, "good");
+   // add grades here
+   createGrade("24.03", 5.75, "Conjugaison", 1);
+   createGrade("24.03", 9, "Production Ecrite", 2);
+   createGrade("16.03", 8, "Compréhension", 1);
 // create grade <=
 
 // create announcement =>
-createAnnouncement("Exam 24-03-21!", "Don't forget to practice!", "#d11e1e");
-createAnnouncement("Introducing Announcements!", "You'll see important information about Francium and your courses here!", "#336fe8");
+
+
+
 // create announcement <=
+if(!document.querySelector("#annbar").hasChildNodes()) {
+   let box = document.createElement("div");
+   let text = document.createTextNode("No recent announcements!");
+   box.classList.add("noAnnouncementClass");
+   box.appendChild(text);
+   document.getElementById("annbar").appendChild(box);
+};
 
 
 const div = d3.select(".thing").append("div").style("display", "flex");
@@ -51,14 +67,15 @@ const grades = div.append("div").style("border", "2px solid black").style("borde
 const description = div.append("div").style("border", "2px solid black").style("border-right", "none").style("border-bottom", "none");
 const average = div.append("div").style("border", "2px solid black");
 // create divs
-const createDiv = (selector, insideData, extra) => selector.selectAll("div").data(dataset[0][insideData]).enter().append("div").style("border-bottom", "2px solid black").style("padding", "3px").text(d => extra ? Number.isInteger(d) ? d + " / 10": d : d);
+const createDiv = (selector, insideData, extra) => selector.selectAll("div").data(dataset[0][insideData]).enter().append("div").style("border-bottom", "2px solid black").style("padding", "3px").text(d => extra ? Number.isInteger(d * 100) ? d + " / 10": d : d);
 createDiv(date, "date");
 createDiv(grades, "grades", "d");
 createDiv(description, "description");
 // average
-let newData = dataset[0].grades.slice(1);
+let newData = dataset[0].coef;
+let roundToHundredth = value => Number(value.toFixed(2));
 average.selectAll("div")
-.data(["Average:", `${newData.reduce((a, c) => a + c) / newData.length} / 10`])
+.data(["Average:", `${roundToHundredth(newData.reduce((a, c) => a + c) / newData.length)} / 10`])
 .enter()
 .append("div")
 .style("margin", "3px")
